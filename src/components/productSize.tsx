e2e943901245a696d00b7b31d8b804e61312d0e2
otw7917 <otw7917@gmail.com>
2023-12-18 16:08:20 +0900

    feat:add product then reset

--- src/components/productSize.tsx
+++ src/components/productSize.tsx
@@ -1,11 +1,17 @@
 const SIZES = ["xl", "lg", "md", "sm", "xs"];
 
-function ProductSize() {
+function ProductSize({ selectedSize }: { selectedSize: string[] }) {
   return (
     <div className='border-2 rounded-md flex justify-center gap-2 p-2'>
       {SIZES.map((size) => (
         <label className='flex gap-1 border-2 p-2 rounded-md'>
-          <input type='checkbox' name='sizes' id='sizes' value={size}></input>
+          <input
+            type='checkbox'
+            name='sizes'
+            id='sizes'
+            value={size}
+            checked={selectedSize.includes(size)}
+          ></input>
           {size}
         </label>
       ))}
